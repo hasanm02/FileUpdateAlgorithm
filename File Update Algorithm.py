074fe8a9136ac210942dade99fdd165c501ecf9f
Hasan <hasanmodest0@gmail.com>
2023-10-26 14:11:28 -0400

    File Update Algorithm.py

--- File Update Algorithm.py
+++ File Update Algorithm.py
@@ -1,6 +1,5 @@
-# In this lab, you're working as a security analyst and you're responsible for developing an algorithm that parses a file containing IP addresses that are allowed to access restricted content and removes addresses that no longer have access.
+# You're responsible for developing an algorithm that parses a file containing IP addresses that are allowed to access restricted content and removes addresses that no longer have access.
 # Define a function named `update_file` that takes in two parameters: `import_file` and `remove_list`
-# and combines the steps you've written in this lab leading up to this
 def update_file(import_file, remove_list):
 
   # Build `with` statement to read in the initial contents of the file
